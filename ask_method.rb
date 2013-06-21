381940c6a0d66c64f31f91853cee50f78c91fdc8
chrisneilsmith <chris.neil.smith@gmail.com>
2013-06-21 15:12:06 -0700

    initial commit

---
+++ ask_method.rb
@@ -0,0 +1,19 @@
+def ask question
+	while true
+		puts question
+		reply = gets.chomp.downcase
+
+			if reply == 'yes'
+				return true
+			end
+			if reply == 'no'
+				return false
+			end
+
+			puts 'Please answer "yes" or "no".'
+		
+	end
+end
+
+tacos = ask "Do you like eating tacos?"
+puts tacos
